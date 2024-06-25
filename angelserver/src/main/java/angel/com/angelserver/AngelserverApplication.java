908e6b663a4f432dd5433ad37e5b740c35e3f3aa
Angel <79809585+AngelOfTheSkyGod@users.noreply.github.com>
2024-06-25 06:52:51 -0500

    initialized java project

---
+++ angelserver/src/main/java/angel/com/angelserver/AngelserverApplication.java
@@ -0,0 +1,13 @@
+package angel.com.angelserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngelserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngelserverApplication.class, args);
+	}
+
+}
---
+++ angelserver/src/test/java/angel/com/angelserver/AngelserverApplicationTests.java
@@ -0,0 +1,13 @@
+package angel.com.angelserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngelserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
